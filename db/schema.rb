0cbdef01b7d9d764b5bfdec53b0d5acd9bcb03f9
Ali Fawad <afawad@creativechaos.co>
2017-08-15 13:35:57 +0500

    commiting to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170815064518) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "interviews", force: :cascade do |t|
     t.string "name"
     t.string "audio_url"
