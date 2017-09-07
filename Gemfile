4d5b61129737f1c160ffd74ed8f4f3936614afe1
Ali Fawad <afawad@creativechaos.co>
2017-09-07 18:15:54 +0500

    refactoring index and show views for interviews, added will_paginate

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'will_paginate', '~> 3.1.0'
+
 
 
 group :development, :test do
--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,9 +1,11 @@
 class InterviewsController < ApplicationController
   def index
+    @disable_header = true
     @interviews = Interview.all
   end
 
   def show
+    @disable_header = true
     @interview = Interview.find(params[:id])
   end
 
